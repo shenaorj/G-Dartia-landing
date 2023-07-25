5999d0cab5f80671ffba18e8b27fd52c287f9ea5
shenaoro <shenaorj@gmail.com>
2023-07-25 17:10:35 -0500

    videos en fila sin repruducción api configurada de yotube

---
+++ src/assets/main.js
@@ -0,0 +1,43 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCfza0jbMW1O_-a0Z86WA_UA&part=snippet%2Cid&order=date&maxResults=9';
+const content = null || document.getElementById('content');
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '706dbb93c8msh9306c3f8cb768d9p1733e4jsn0b388437d603',
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+};
+
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full ">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-primary">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+        `).slice(0, 4).join('')}
+          `;
+       content.innerHTML = view;   
+    } catch (error) {
+        console.log(error);
+    }
+})();
