1b12ed6438171cee98d39a447669c5044b8f0cb6
shenaoro <shenaorj@gmail.com>
2023-07-25 18:01:31 -0500

    videos en 2 columnas fncionales con links pero sin estilose

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,12 +1,12 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCfza0jbMW1O_-a0Z86WA_UA&part=snippet%2Cid&order=date&maxResults=9';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCfza0jbMW1O_-a0Z86WA_UA&part=snippet%2Cid&order=date&maxResults=8';
 const content = null || document.getElementById('content');
 
 const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '706dbb93c8msh9306c3f8cb768d9p1733e4jsn0b388437d603',
-        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '706dbb93c8msh9306c3f8cb768d9p1733e4jsn0b388437d603',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
 };
 
 
@@ -23,6 +23,7 @@ async function fetchData(urlApi) {
         let view = `
         ${videos.items.map(video => `
         <div class="group relative">
+        <a target=_blank href="https://www.youtube.com/watch?v=${video.id.videoId}">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
           <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full ">
@@ -34,7 +35,7 @@ async function fetchData(urlApi) {
           </h3>
         </div>
       </div>
-        `).slice(0, 4).join('')}
+        `)}
           `;
        content.innerHTML = view;   
     } catch (error) {
